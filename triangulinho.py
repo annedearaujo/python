5fc68efc79fd7a53b2eef0782401c5d606cce2d1
Anne Araújo <108763060+ennaraujo@users.noreply.github.com>
2022-09-19 22:25:00 -0300

    Triângulo equilátero: true or false.
    Programa que lê 3 valores correspondentes aos lados de um triângulo informados pelo usuário e escreve True se o triângulo formado por esses 3 lados é equilátero; False caso contrário.

---
+++ triangulinho.py
@@ -0,0 +1,10 @@
+print('imagine um triângulo e então informe as medidas dele')
+
+I = float(input('a: '))
+II = float(input('b: '))
+III = float(input('c: '))
+
+print('é equilátero? ')
+
+print (I==II==III)
+    
